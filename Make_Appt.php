35d8740bb07e5802ba7cb9c658bf7cbb5f2215c2
root <root@zergling7.cs.utsarr.net>
2018-11-30 17:41:21 +0000

    apptest

---
+++ Make_Appt.php
@@ -0,0 +1,26 @@
+<?php
+include("ActivityBar.php");
+echo'
+<html>
+<div class="main">
+<h1>Account Details</h1>
+<body>
+<h2>General: </h2>
+<p>Name on account:</p>
+<p>Email on account(log in): </p>
+<h2>Personal</h2>
+<p>SSN:</p>
+<p>Birthdays</p>
+<form method="post">
+<label>pass: </label>
+<input type="text" placeholder="new" name="pass"><br/>
+</form>
+</div>
+</body>
+</html>
+
+';
+
+
+?>
+
