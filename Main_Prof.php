998c8847511b206884fc915f6e17e79dc4b7e231
root <root@zergling7.cs.utsarr.net>
2018-12-03 16:18:51 +0000

    in class 10:18

--- Main_Prof.php
+++ Main_Prof.php
@@ -5,11 +5,12 @@ echo'
 <body>
 <div class = main>
 <p> Here is your main profile page, you can do the following:<br/>
-View account Details<br/>
-Make appointments<br/>
-View Appointments<br/>
-Cancel Appointments<br/>
-Update Information<br/>
+<ul>
+	<li>View account Details</li>
+	<li>Make appointments</li>
+	<li>View Appointments</li>
+	<li>Cancel Appointments<li>
+	<li>Update Information</li>
 </p>
 </div>
 
