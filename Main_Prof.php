2d3a14f7e8f2354ad5ccf0d66441c29eb6cd8eca
root <root@zergling7.cs.utsarr.net>
2018-12-03 16:22:27 +0000

    what to do for permissions

--- Main_Prof.php
+++ Main_Prof.php
@@ -1,6 +1,12 @@
 <?php
 
 include("ActivityBar.php");
+//grab their access level from session
+//print different bullet points 
+//for 1 add make/cancel/view appts
+//for 2 add all those but for like front desk
+//for 3 all these also add view app description (nurses and such)
+//for 4 add alll + edit personal information+ make appts + write medications + chat or something
 echo'
 <body>
 <div class = main>
