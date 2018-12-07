68a5902a7c703fa625745afbb75766deb8a2c71e
scottlandunderwood <fyw759@my.utsa.edu>
2018-12-07 12:28:28 -0600

    added calendar to select date for appointments

--- Make_Appt.php
+++ Make_Appt.php
@@ -11,7 +11,33 @@ echo'
 <input type="text" placeholder="new" name="pass"><br/>
 </form>
 Calender:';
-echo draw_calendar(12,2018);
+//echo draw_calendar(12,2018);
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>jQuery UI Datepicker - Icon trigger</title>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script>
+  $( function() {
+    $( "#datepicker" ).datepicker({
+      showOn: "button",
+      buttonImage: "images/calendar.gif",
+      buttonImageOnly: true,
+      buttonText: "Select date"
+    });
+  } );
+  </script>
+</head>
+
+
+<p>Date: <input type="text" id="datepicker"></p>
+
+
+
+
 echo'
 </div>
 </body>
