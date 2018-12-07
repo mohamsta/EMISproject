5dc83a3cd18097d9abf60b91700d985a220ea647
scottlandunderwood <fyw759@my.utsa.edu>
2018-12-07 16:28:53 -0600

    scottland added calendar to select date for appointments

--- Make_Appt.php
+++ Make_Appt.php
@@ -1,51 +1,47 @@
 <?php
 include("ActivityBar.php");
 include("moAlender.php");
+?>
 
-echo'
-<html>
+<html lang ="en">
 <div class="main">
 <body>
-<h3>Pick a date to schedule an appointment!</h3>
+<h3>Pick date to schedule an appointment now!</h3>
 <form method="post">
 <label>Date: </label>
 <input type="text" placeholder="new" name="pass"><br/>
 </form>
-Calender:';
-//echo draw_calendar(12,2018);
-<head>
+<!--alender:';-->
+<!--//echo draw_calendar(12,2018);-->
+<!doctype html>
+
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>jQuery UI Datepicker - Icon trigger</title>
+  <title>jQuery UI Datepicker - Display button bar</title>
   <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
   <link rel="stylesheet" href="/resources/demos/style.css">
   <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
   <script>
-  $( function() {
+$( function() {
     $( "#datepicker" ).datepicker({
-      showOn: "button",
-      buttonImage: "images/calendar.gif",
-      buttonImageOnly: true,
-      buttonText: "Select date"
+      showButtonPanel: true
     });
   } );
   </script>
-</head>
 
 
 <p>Date: <input type="text" id="datepicker"></p>
 
 
 
-
-echo'
 </div>
 </body>
 </html>
 
-';
 
 
-?>
+
+
+
 
