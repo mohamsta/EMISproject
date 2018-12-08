9ddd79bab120dc2d6a5fe11011733d467ba740d1
scottlandunderwood <fyw759@my.utsa.edu>
2018-12-08 16:14:24 -0600

    scottland-revised issue with diff

--- form.php
+++ form.php
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Simple Form</title>
-    </head>
-    <body>
-        <h2>A Simple Form</h2>
- 
-        <!-- A simple form created using HTML method -->
-        <form action="submit.php" method="POST">
-            <input type="text" name="Fname" value="" placeholder="First Name"><br/>
-            <input type="text" name="Lname" value="" placeholder="Last Name"><br/>
-            <input type="text" name="Username" value="" placeholder="Username"><br/>
-            <input type="password" name="Password" value="" placeholder="Password"><br/>
-            <input type="submit" name="Submit">
-        </form>
-        <!-- End of the form -->
- 
-    </body>
-</html>
-=======
 <?php
 /**
  * Created by PhpStorm.
@@ -48,5 +26,3 @@
 
 </body>
 </html>
-
->>>>>>> 4af3c7729656653d2f82a3eaab646537c912e3fb
--- submit.php
+++ submit.php
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Simple Form</title>
-    </head>
-    <body>
-        <h2>Thank You For Submitting The Form</h2>
- 
-        <!-- Getting the output from the form value provided through 'form.php' -->
-        First Name: <?php echo $_POST['Fname']; // displaying the value of First Name provided in 'Form.php' while submitting the form ?><br/>
-        Last Name: <?php echo $_POST['Lname']; // displaying the value of Last Name provided in 'Form.php' while submitting the form ?><br/>
-        Username: <?php echo $_POST['Username']; // displaying the value of Username provided in 'Form.php' while submitting the form ?><br/>
-        Password: <?php echo $_POST['Password']; // displaying the value of Password provided in 'Form.php' while submitting the form ?><br/>
-        <!-- End of getting the value from the submitted form -->
-    </body>
-=======
 <?php
 /**
  * Created by PhpStorm.
@@ -38,5 +21,4 @@ Username: <?php echo $_POST['Username']; // displaying the value of Username pro
 Password: <?php echo $_POST['Password']; // displaying the value of Password provided in 'Form.php' while submitting the form ?><br/>
 <!-- End of getting the value from the submitted form -->
 </body>
->>>>>>> 4af3c7729656653d2f82a3eaab646537c912e3fb
 </html>
