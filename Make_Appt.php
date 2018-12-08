3ebded92debfe0354b47014ee03e782e62d31f8e
scottlandunderwood <fyw759@my.utsa.edu>
2018-12-08 01:03:41 -0600

    scottland-added button to display form for appt time

--- Make_Appt.php
+++ Make_Appt.php
@@ -5,39 +5,43 @@ include("moAlender.php");
 
 <html lang ="en">
 <div class="main">
-<body>
-<h3>Pick date to schedule your appointment now!</h3>
-<form method="post">
-<label>Date: </label>
-<input type="text" placeholder="new" name="pass"><br/>
-</form>
-<!--alender:';-->
-<!--//echo draw_calendar(12,2018);-->
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>jQuery UI Datepicker - Display button bar</title>
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <link rel="stylesheet" href="/resources/demos/style.css">
-    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <script>
-        $( function() {
-            $( "#datepicker" ).datepicker({
-                showButtonPanel: true
+    <body>
+    <h3>Pick date to schedule appointment now!</h3>
+    <form method="post">
+        <label>Date: </label>
+        <input type="text" placeholder="new" name="pass"><br/>
+    </form>
+    <!--alender:';-->
+    <!--//echo draw_calendar(12,2018);-->
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>jQuery UI Datepicker - Display button bar</title>
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <link rel="stylesheet" href="/resources/demos/style.css">
+        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        <script>
+            $( function() {
+                $( "#datepicker" ).datepicker({
+                    showButtonPanel: true
+                });
             });
-        } );
-    </script>
-</head>
+        </script>
 
-<p>Date: <input type="text" id="datepicker"></p>
+    </head>
+    <p>Date: <input type="text" id="datepicker"></p>
+
+    <button onclick="myFunction()">Set Appointment Time</button>
+
+
+    
 
 </div>
 </body>
 </html>
-
 
 
 
